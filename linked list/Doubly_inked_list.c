b1eea57ad82c45b0e2fd188d3403c95a05196bb7
Santosh Bhor <125242679+sbhor007@users.noreply.github.com>
2023-09-13 17:18:36 +0530

    Add files via upload

---
+++ linked list/Doubly_inked_list.c
@@ -0,0 +1,292 @@
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+
+struct dlink
+{
+    int data;
+    struct dlink *prevAdd, *nextAdd;
+};
+typedef struct dlink node;
+node *start = NULL;
+node *newnode, *temp, *next, *prev;
+
+int menu()
+{
+    int no;
+    printf("\n1) Create Doubly Linked List.");
+    printf("\n----------------------------");
+    printf("\n2) Insert First.");
+    printf("\n3) Insert Last.");
+    printf("\n4) Insert Middle.");
+    printf("\n----------------------------");
+    printf("\n5) Delete First.");
+    printf("\n6) Delete Last.");
+    printf("\n7) Delete Middle.");
+    printf("\n----------------------------");
+    printf("\n8) Count node");
+    printf("\n9) Traiverse left to right.");
+    printf("\n10) Traiverse right to lrft.");
+    printf("\n----------------------------");
+    printf("\n11) Exit");
+    printf("\n----------------------------");
+    printf("\nEnter a Choice : ");
+    scanf("%d", &no);
+    return no;
+}
+
+node *getnode()
+{
+    newnode = (node *)malloc(sizeof(node));
+    printf("\nEnter Data : ");
+    scanf("%d", &newnode->data);
+    newnode->nextAdd = NULL;
+    newnode->prevAdd = NULL;
+    return newnode;
+}
+
+int count()
+{
+    int ctr = 1;
+    for (temp = start; temp->nextAdd != NULL; temp = temp->nextAdd)
+        ctr++;
+    return ctr;
+}
+
+void createList(int no)
+{
+
+    for (int i = 1; i <= no; i++)
+    {
+        newnode = getnode();
+        if (start == NULL)
+            start = newnode;
+        else
+        {
+            temp = start;
+            while (temp->nextAdd != NULL)
+                temp = temp->nextAdd;
+            temp->nextAdd = newnode;
+            newnode->prevAdd = temp;
+        }
+    }
+
+    printf("\nList created.....");
+}
+
+void travalLeftToRight()
+{
+    temp = start;
+    printf("\nList data are : ");
+    if (start == NULL)
+        printf("Empty.....!");
+    else
+    {
+        while (temp != NULL)
+        {
+            printf("%5d", temp->data);
+            temp = temp->nextAdd;
+        }
+    }
+}
+
+void TravelsRightToLeft()
+{
+    temp = start;
+    printf("\nList Data in reverse order is : ");
+    if (start == NULL)
+        printf("Empty.......!");
+    else
+    {
+        while (temp->nextAdd != NULL)
+            temp = temp->nextAdd;
+    }
+    while (temp != NULL)
+    {
+        printf("%5d", temp->data);
+        temp = temp->prevAdd;
+    }
+}
+
+void insertFirst()
+{
+    newnode = getnode();
+    if (start == NULL)
+    {
+        start = newnode;
+        printf("\nNode inserted at first position.");
+    }
+    else
+    {
+        newnode->nextAdd = start;
+        start->prevAdd = newnode;
+        start = newnode;
+        printf("\nNode inserted at first position.");
+    }
+}
+
+void insertLast()
+{
+    newnode = getnode();
+    if (start == NULL)
+    {
+        start = newnode;
+        printf("\nNode inserted at Last position.");
+    }
+    else
+    {
+        temp = start;
+        while (temp->nextAdd != NULL)
+            temp = temp->nextAdd;
+        temp->nextAdd = newnode;
+        newnode->prevAdd = temp;
+        printf("\nNode inserted at Last position.");
+    }
+}
+
+void insertMiddle()
+{
+    int i, pos, ctr;
+    if (start == NULL)
+    {
+        printf("\nInsertion not posible.");
+        printf("\nEmpty is List....");
+    }
+    else
+    {
+        ctr = count();
+        printf("\nEnter the position to insert : ");
+        scanf("%d", &pos);
+        if (pos > 1 && pos <= ctr)
+        {
+            newnode = getnode();
+            temp = start;
+            for (i = 2; i < pos; i++)
+            {
+                temp = temp->nextAdd;
+            }
+            prev = temp->prevAdd;
+            newnode->prevAdd = prev;
+            prev->nextAdd = newnode;
+            newnode->nextAdd = temp;
+            temp->prevAdd = newnode;
+            printf("\nElement Inserted.");
+        }
+        else if (pos == 1)
+            insertFirst();
+        else
+        {
+            printf("\nInvalid Position.");
+        }
+    }
+}
+
+void deleteFirst()
+{
+    temp = start;
+    start = temp->nextAdd;
+    printf("\nDeleted Number is : %d ", temp->data);
+    free(temp);
+}
+
+void deleteLast()
+{
+    temp = start;
+    while (temp->nextAdd != NULL)
+        temp = temp->nextAdd;
+    prev = temp->prevAdd;
+    prev->nextAdd = NULL;
+    printf("\nDeleted element is : %d ", temp->data);
+    free(temp);
+}
+
+void deleteMiddle()
+{
+    int i, pos, ctr;
+    if (start == NULL)
+    {
+        printf("\nDeletion not posible.");
+        printf("\nEmpty is List....");
+    }
+    else
+    {
+        ctr = count();
+        printf("\nEnter the position to insert : ");
+        scanf("%d", &pos);
+        if (pos > 1 && pos <= ctr)
+        {
+            temp = start;
+            for (i = 2; i < pos; i++)
+            {
+                temp = temp->nextAdd;
+            }
+            prev = temp->prevAdd;
+            next = temp->nextAdd;
+            prev->nextAdd = next;
+            next->prevAdd = prev;
+            printf("\nDeleted Element is : %d", temp->data);
+            free(temp);
+        }
+        /*else if (pos == 1)
+            deleteFirst();*/
+        else
+        {
+            printf("\nInvalid Position.");
+        }
+    }
+}
+
+int main()
+{
+    int no;
+    while (1)
+    {
+        switch (menu())
+        {
+        case 1:
+            printf("\nHow many Elements to store : ");
+            scanf("%d", &no);
+            createList(no);
+            getch();
+            break;
+        case 2:
+            insertFirst();
+            getch();
+            break;
+        case 3:
+            insertLast();
+            getch();
+            break;
+        case 4:
+            insertMiddle();
+            getch();
+            break;
+        case 5:
+            deleteFirst();
+            getch();
+            break;
+        case 6:
+            deleteLast();
+            getch();
+            break;
+        case 7:
+            deleteMiddle();
+            getch();
+            break;
+        case 8:
+            printf("\nNO of List Elements is : %d", count());
+            getch();
+            break;
+        case 9:
+            travalLeftToRight();
+            getch();
+            break;
+        case 10:
+            TravelsRightToLeft();
+            getch();
+            break;
+        default:
+            exit(0);
+        }
+    }
+}
---
+++ linked list/Singli_linked_list.c
@@ -0,0 +1,295 @@
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+struct linkedList
+{
+    int data;
+    struct linkedList *next;
+};
+typedef struct linkedList node;
+node *start = NULL;
+
+int menu()
+{
+    int no;
+    printf("\n1) Create List.");
+    printf("\n----------------------------");
+    printf("\n2) insert node at first.");
+    printf("\n3) insert node at last.");
+    printf("\n4) insert node at Middle.");
+    printf("\n----------------------------");
+    printf("\n5) Delete at first node.");
+    printf("\n6) Delete at Last node.");
+    printf("\n7) Delete at Middele node.");
+    printf("\n-----------------------------");
+    printf("\n8) Count nodes.");
+    printf("\n9) Display List.");
+    printf("\n10) reverse List.");
+    printf("\n-----------------------------");
+    printf("\n11) exit.");
+    printf("\n-------------------------------");
+    printf("\nEnter a Choise : ");
+    scanf("%d", &no);
+    return no;
+}
+node *getnode()
+{
+    node *newnode = (node *)malloc(sizeof(node));
+    printf("\nEnter Data : ");
+    scanf("%d", &newnode->data);
+    newnode->next = NULL;
+    return newnode;
+}
+
+void createList(int no)
+{
+    node *newnode;
+    node *temp;
+    for (int i = 1; i <= no; i++)
+    {
+        newnode = getnode();
+        if (start == NULL)
+            start = newnode;
+        else
+        {
+            temp = start;
+            while (temp->next != NULL)
+                temp = temp->next;
+            temp->next = newnode;
+        }
+    }
+    printf("\nList created");
+    getch();
+}
+int counter()
+{
+    int ctr = 0;
+    node *temp = start;
+    while (temp != NULL)
+    {
+        ctr++;
+        temp = temp->next;
+    }
+    return ctr;
+}
+void insertFirst()
+{
+    node *newnode = getnode();
+    if (start == NULL)
+        start = newnode;
+    else
+    {
+
+        newnode->next = start;
+        start = newnode;
+    }
+    printf("\nEliment inserted at first position.");
+    getch();
+}
+void insertLast()
+{
+    node *newnode = getnode();
+    node *temp;
+    if (start == NULL)
+        start = newnode;
+    else
+    {
+        temp = start;
+        while (temp->next != NULL)
+            temp = temp->next;
+        temp->next = newnode;
+    }
+    printf("\nElement inserted at last Position");
+    getch();
+}
+void display()
+{
+    node *temp = start;
+    printf("\nList Data are : ");
+    if (start == NULL)
+        printf("Empty.....!");
+    else
+    {
+        while (temp != NULL)
+        {
+            printf("%5d", temp->data);
+            temp = temp->next;
+        }
+    }
+    getch();
+}
+void insertMiddle()
+{
+    node *newnode, *prev, *temp;
+    int i, pos, ctr;
+    printf("Enter the position to insert element : ");
+    scanf("%d", &pos);
+    ctr = counter();
+    if (pos > 0 && pos <= ctr)
+    {
+        newnode = getnode();
+        temp = start;
+        for (i = 0; i < pos; i++)
+        {
+            prev = temp;
+            temp = temp->next;
+        }
+        prev->next = newnode;
+        newnode->next = temp;
+        printf("\nElement inserted successfully.");
+    }
+    else
+    {
+        printf("\ninvalid position.");
+    }
+    getch();
+}
+void deleteFirst()
+{
+    node *temp = start;
+    if (start != NULL)
+    {
+        start = temp->next;
+        printf("\nDeleted Element is : %d", temp->data);
+        free(temp);
+        printf("\nElememt deleted successfully.");
+        getch();
+    }
+    else
+    {
+        printf("\nList not exist");
+        getch();
+    }
+}
+void deleteLast()
+{
+    node *temp, *prev;
+    if (start != NULL)
+    {
+        temp = start;
+        while (temp->next != NULL)
+        {
+            prev = temp;
+            temp = temp->next;
+        }
+        prev->next = NULL;
+        printf("\nDeleted Element is : %d", temp->data);
+        free(temp);
+        printf("\nElememt deleted successfully.");
+        getch();
+    }
+    else
+    {
+        printf("\nList not exist.");
+        getch();
+    }
+}
+void deleteMiddle()
+{
+    node *temp, *prev;
+    int i, pos, ctr = counter();
+    if (start != NULL)
+    {
+        printf("\nEnter the Position to delete element : ");
+        scanf("%d", &pos);
+        if (pos > 0 && pos <= ctr)
+        {
+            temp = start;
+            i = 1;
+            while (i < pos)
+            {
+                prev = temp;
+                temp = temp->next;
+                i++;
+            }
+            prev->next = temp->next;
+            printf("\nDeleted element is : %d", temp->data);
+            free(temp);
+            printf("\nElememt deleted successfully.");
+            getch();
+        }
+        else
+        {
+            printf("\ninvalid position.");
+            getch();
+        }
+    }
+    else
+    {
+        printf("\nList Not Exist.");
+        getch();
+    }
+}
+void reverseList()
+{
+    node *a[500];
+    node *temp = start;
+    int i = 0;
+    if (start != NULL)
+    {
+        while (temp != NULL)
+        {
+            a[i] = temp;
+            temp = temp->next;
+            i++;
+        }
+        i--;
+        while (i >= 0)
+        {
+            printf("%5d", a[i]->data);
+            i--;
+        }
+        getch();
+    }
+    else
+    {
+        printf("\n lisr data are : Empty...!");
+        getch();
+    }
+}
+int main()
+{
+    int no;
+    while (1)
+    {
+        switch (menu())
+        {
+        case 1:
+            printf("\nHow many elements in a list : ");
+            scanf("%d", &no);
+            createList(no);
+            break;
+        case 2:
+            insertFirst();
+            break;
+        case 3:
+            insertLast();
+            break;
+        case 4:
+            insertMiddle();
+            break;
+        case 5:
+            deleteFirst();
+            break;
+        case 6:
+            deleteLast();
+            break;
+        case 7:
+            deleteMiddle();
+            break;
+        case 8:
+            printf("\nNo of List elements is : %d", counter());
+            getch();
+            break;
+        case 9:
+            display();
+            break;
+        case 10:
+            reverseList();
+            break;
+        default:
+            exit(0);
+        }
+    }
+    return 0;
+}
